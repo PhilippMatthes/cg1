6e510e3ac58a1c619a44e1688d7a754169ff3224
FKasy <kaestner.fe@gmail.com>
2018-12-17 02:02:25 +0100

    Solution for Exercise 2.2.2 a) and b)

--- exercise3/src/SurfaceArea.cpp
+++ exercise3/src/SurfaceArea.cpp
@@ -10,6 +10,27 @@ float ComputeSurfaceArea(const HEMesh& m)
 {
 	float area = 0;
 	/* Task 2.2.2 */
-	std::cout << "Area computation is not implemented." << std::endl;
+	// iterator over all faces
+	for (OpenMesh::PolyConnectivity::FaceIter
+				f_it=m.faces_begin();
+				f_it!=m.faces_end();
+				++f_it){
+		std::cout << "Iterating over Face " << f_it.operator*().idx() << std::endl;
+
+		auto normal = m.calc_face_normal(f_it.operator*());
+
+		// iterator over all halfedges of a face
+		for (OpenMesh::PolyConnectivity::FaceHalfedgeIter
+					 fh_iter=m.cfh_iter(f_it.operator*());
+					 fh_iter.is_valid();
+					 ++fh_iter)
+		{
+			auto from = m.point(m.from_vertex_handle(fh_iter.operator*()));
+			auto to = m.point(m.to_vertex_handle(fh_iter.operator*()));
+			std::cout << "Iterating over Halfedge from: " << from << " to " << to << std::endl;
+			area += OpenMesh::dot(OpenMesh::cross(from, to)/2, normal);
+		}
+	}
+
 	return area;
 }
